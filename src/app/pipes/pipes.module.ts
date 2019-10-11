930e5c3ba43cc323a524571eb746c732fb3ec26d
Phils Garcia <eivanphils@gmail.com>
2019-10-11 09:23:45 -0300

    agregado filtro

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ImagenPipe } from './imagen.pipe';
+import { FilterMoviesPipe } from './filter-movies.pipe';
 
 
 
 @NgModule({
-  declarations: [ImagenPipe],
-  exports: [ImagenPipe],
+  declarations: [
+    ImagenPipe,
+    FilterMoviesPipe],
+  exports: [
+    ImagenPipe,
+    FilterMoviesPipe],
   imports: [
     CommonModule
   ]
